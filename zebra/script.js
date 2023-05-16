f8d91661743eba4e8bb720dc2cdddb823e213874
Tae Yun <73193826+koolung@users.noreply.github.com>
2023-05-16 15:28:50 -0400

    quantity counter added

--- zebra/script.js
+++ zebra/script.js
@@ -26,9 +26,20 @@ function goToNewPage()
     window.location = url;
 }
 
-  /*
-  var url = document.getElementById('option').value;
-  if(url != 'none') {
-      window.location = url;
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
   }
-  */
+  
+  function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+      value = isNaN(value) ? 0 : value;
+      value--;
+      input.value = value;
+    }
+  }
