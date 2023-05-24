930bbd672e353c514b78bf59966c4bb6991b9e52
Tae Yun <73193826+koolung@users.noreply.github.com>
2023-05-24 15:55:02 -0400

    php

---
+++ hero.php
@@ -0,0 +1,12 @@
+  <!-- Hero Section  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>Customize<span></span></h1>
+        <h1>Curtains & Blinds<span></span></h1>
+        <h1>At Your Fingertips<span></span></h1>
+        <a href="shop.html" type="button" class="cta"><b>Shop Now</b></a>
+      </div>
+    </div>
+  </section>
+  <!-- End Hero Section  -->
