8af1f4e88ab32ca9f603bdd619de603a5384929b
Tae Yun <73193826+koolung@users.noreply.github.com>
2023-05-26 12:20:00 -0400

    sync

---
+++ contact.php
@@ -0,0 +1,36 @@
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container" style="min-height: 30vh;">
+      <div>
+        <h1 class="section-title">Contact <span>info</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><a href="tel:+1 902 9890740"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></a></div>
+          <div class="contact-info">
+            <a href="tel:+1 902 9890740">
+            <h1>Phone</h1>
+            <h2>+1 902 989 0740</h2>
+            </a>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><a href="mailto:mifudecor@gmail.com"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></a></div>
+          <div class="contact-info">
+            <a href="mailto:mifudecor@gmail.com">
+            <h1>Email</h1>
+            <h2>mifudecor@gmail.com</h2>
+            </a>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Address</h1>
+            <h2>201 Brookline Plaza, Bedford, NS B4B 2E6</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
---
+++ header.php
@@ -0,0 +1,39 @@
+ 
+ 
+ <!-- Header -->
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Mifu Draperies & Curtains</title>
+</head>
+  <section id="header" onload="transparentBg">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="index.html">
+            <img class="logo" src="./img/logo1.png" alt="logo">
+          </a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger">
+            <div class="bar"></div>
+          </div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Services</a></li>
+            <li><a href="shop.html" data-after="Projects">Pruducts</a></li>
+            <li><a href="corporate.html" data-after="Corporate">Corporate</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+            <li><a href="#contact" data-after="Contact">Log In</a></li>
+            <li><a href="cart./index.html" data-after="Cart" class="bagicon"><img class="bag" src="img/bag.png" alt="cart"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Header -->
+
+  <script src="./app.js"></script>
